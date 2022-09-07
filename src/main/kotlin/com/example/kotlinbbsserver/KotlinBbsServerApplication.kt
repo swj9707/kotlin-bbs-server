644c16976f48aad62ed4c62c6d5464364245aa6a
swj9707 <swj1265@naver.com>
2022-09-07 23:25:38 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/kotlinbbsserver/KotlinBbsServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinbbsserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBbsServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinBbsServerApplication>(*args)
+}
